88d200f11aec90e79254b832273db80f4b093f65
dgeorgiev436 <68106371+dgeorgiev436@users.noreply.github.com>
2022-08-10 12:46:58 +0100

    Passing functions around

--- lib/question.dart
+++ lib/question.dart
@@ -4,11 +4,20 @@ class Question extends StatelessWidget {
   // Final tells Dart that value will not change after initialization
   final String questionText;
 
-// Constructor
+// Class Constructor
   Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity, // Width of container equals to full width of the screen
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        // Styling
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
